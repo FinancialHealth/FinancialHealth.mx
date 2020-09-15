6a52910d21e24c30a47426f5b5327c5280320d37
FinancialHealth <sistemasfhmx@gmail.com>
2020-09-15 14:44:23 -0500

    Mariaromofig mail added to House Form

--- js/main.js
+++ js/main.js
@@ -224,12 +224,5 @@
             $('.modal:visible').each(centerModal);
         });
 
-        $(function(){
-            var $select = $(".1-100");
-            for (i=1;i<=100;i++){
-                $select.append($('<option></option>').val(i).html(i))
-            }
-        });
-
 
 })(jQuery);
